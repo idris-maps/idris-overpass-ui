1264be011a138313e937a8dc01b346787cc92e3f
Idris <idris.maps@gmx.ch>
2017-03-17 08:20:17 +0100

    style

--- main.js
+++ main.js
@@ -4,8 +4,21 @@ import Menu from './lib/components/Menu';
 import map from './lib/map'
 
 window.onload = function() {
+	resizeMap()
 	map.init('map', function() {
 		ReactDOM.render(<Menu/>, document.getElementById('menu'))
 	})
+}
+
+window.onresize = function() {
+	resizeMap()
+}
+
+function resizeMap() {
+	var map = document.getElementById('map')
+	var h = window.innerHeight
+	var headH = 75
+	console.log('resize', h, headH, (h - headH) + 'px')
+	map.style.height = (h - headH) + 'px'
 }
 
